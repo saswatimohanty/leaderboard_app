8277ef8ec6b6620868589791c2d2778be3ef6924
Saswati Mohanty <msaswati90@gmail.com>
2016-07-28 13:36:29 +0530

    Removed unwanted files

--- db/migrate/20160719103227_add_public_repos_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPublicReposToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :public_repos, :integer
-  end
-end
--- db/migrate/20160726073548_add_followers_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddFollowersToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :followers, :integer
-  end
-end
